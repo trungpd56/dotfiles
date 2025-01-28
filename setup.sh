508929e14f7962b09e45683944b2d448a5ee7b27
trungpd56 <trungpd56@gmail.com>
2025-01-28 23:06:52 +0700

    my personal dotfiles

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+dotfilesDir=$(pwd)
+
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest}
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+linkDotfile .vim
+linkDotfile .tmux.conf
+linkDotfile .gitconfig
+
+sudo apt -y install tmux tree rename curl git
+grep 'myalias' ~/.bashrc || echo 'source ~/dotfiles/myalias' >> ~/.bashrc
+# curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+# grep 'zoxide' ~/.bashrc || echo 'eval "$(zoxide init bash --cmd cd)"' >> ~/.bashrc
+source ~/.bashrc
+curl -sL install-node.vercel.app/lts | sudo bash
+
+# cd /tmp && wget https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb && dpkg -i git-delta_0.17.0_amd64.deb
+# sudo add-apt-repository ppa:jonathonf/vim
